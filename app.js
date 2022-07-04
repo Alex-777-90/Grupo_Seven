5a9f30e053434e058c451f437cc1e608a8a8e5b1
Alex <alxnvn@yahoo.com.br>
2022-07-04 19:57:14 -0300

    criando pastas no padrão MVC e subindo servidor app.js

---
+++ app.js
@@ -0,0 +1,21 @@
+//entry point ---------------------------------------------------------------------//
+const express = require("express");
+const app = express();
+
+
+
+
+
+
+
+
+
+
+
+app.listen(3080,()=>{
+console.log("Servidor rodando na porta 3080 ,no endereço http://localhost:3080/ ")
+})
+
+module.exports = app
+//entry point ---------------------------------------------------------------------//
+
