a54df1a07122b129172e5b6c6bdafba021a3e2a5
Alex <alxnvn@yahoo.com.br>
2022-07-13 01:13:00 -0300

    atualização do forEach e rotas

--- src/controllers/controllersPaginas.js
+++ src/controllers/controllersPaginas.js
@@ -10,6 +10,26 @@ const productsFarmacia2 = JSON.parse(fs.readFileSync(productsFarmaciaPath2, 'utf
 
 //const farmacia fim -------------------------------------------------------------------------------------////////
 
+
+//const pet inicio -------------------------------------------------------------------------------------////////
+const productsPetPath = path.join(__dirname, '../database/petDataBase.json');
+const productsPet = JSON.parse(fs.readFileSync(productsPetPath, 'utf-8'));
+
+const productsPetPath2 = path.join(__dirname, '../database/petDataBase2.json');
+const productsPet2 = JSON.parse(fs.readFileSync(productsPetPath2, 'utf-8'));
+
+//const pet fim -------------------------------------------------------------------------------------////////
+
+
+//const variedades inicio -------------------------------------------------------------------------------------////////
+const productsVariedadesPath = path.join(__dirname, '../database/variedadesDataBase.json');
+const productsVariedades = JSON.parse(fs.readFileSync(productsVariedadesPath, 'utf-8'));
+
+const productsVariedadesPath2 = path.join(__dirname, '../database/variedadesDataBase2.json');
+const productsVariedades2 = JSON.parse(fs.readFileSync(productsVariedadesPath2, 'utf-8'));
+
+//const variedades fim -------------------------------------------------------------------------------------////////
+
 const toThousand = n => n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 
 
@@ -20,11 +40,11 @@ const controlePaginas = {
   },
 
   pet:(req,res) =>{
-     return res.render("pet");
+     return res.render("pet",{ productsPet,productsPet2,toThousand });
   },
 
   variedades:(req,res) =>{
-    return res.render("variedades");
+    return res.render("variedades", {productsVariedades, productsVariedades2 ,toThousand });
   },
 
   home:(req,res) =>{
--- src/routes/routerPaginas.js
+++ src/routes/routerPaginas.js
@@ -4,7 +4,7 @@ var controlePaginas = require("../controllers/controllersPaginas");
 
 router.get("/farmacia",controlePaginas.farmacia);
 router.get("/pet",controlePaginas.pet);
-router.get(" ",controlePaginas.home);
+router.get("",controlePaginas.home);
 router.get("/variedades",controlePaginas.variedades);
 router.get("/formulario",controlePaginas.formulario);
 router.post("/formulario",controlePaginas.dadosForm);
