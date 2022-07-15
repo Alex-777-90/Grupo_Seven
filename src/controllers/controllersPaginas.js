bf6034398e4c26f8e797a53fc6f33fc253efbd95
Alex <alxnvn@yahoo.com.br>
2022-07-15 14:38:33 -0300

    Atualização das rotas e do arquivo JSON

--- src/controllers/controllersPaginas.js
+++ src/controllers/controllersPaginas.js
@@ -1,39 +1,55 @@
 const fs = require('fs');
 const path = require('path');
 
-//const farmacia inicio -------------------------------------------------------------------------------------////////
-const productsFarmaciaPath = path.join(__dirname, '../database/farmaciaDataBase.json');
-const productsFarmacia = JSON.parse(fs.readFileSync(productsFarmaciaPath, 'utf-8'));
 
-const productsFarmaciaPath2 = path.join(__dirname, '../database/farmaciaDataBase2.json');
-const productsFarmacia2 = JSON.parse(fs.readFileSync(productsFarmaciaPath2, 'utf-8'));
-
-//const farmacia fim -------------------------------------------------------------------------------------////////
+const allProdutosPath = path.join(__dirname, '../database/allProdutos.json'); //pega os dados do JSON
+const allProducts = JSON.parse(fs.readFileSync(allProdutosPath, 'utf-8')); // transforma o JSON e uma array 
 
 const toThousand = n => n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 
 const controlePaginas = {
  
-  farmacia:(req,res) =>{
-    return res.render("farmacia",{productsFarmacia , productsFarmacia2, toThousand});
-  },
+ // farmacia inicio -------------------------------------------------------------------------------------////////
+ farmacia:(req,res) =>{
+
+  let productsFarmacia = allProducts.filter(function(value){
+
+    return value.sector == "farmacia" && value.item <= 3
+       
+  });
+
+
+  let productsFarmacia2 = allProducts.filter(function(value){
+
+    return value.sector == "farmacia" && value.item >= 4
+
+  });
+
+
+  return res.render("farmacia",{productsFarmacia , productsFarmacia2, toThousand});
+},
+// farmacia fim -----------------------------------------------------------------------------------------////////
 
   pet:(req,res) =>{
     return res.render("pet");
  },
 
  variedades:(req,res) =>{
+
    return res.render("variedades");
  },
 
  home:(req,res) =>{
+
    return res.render("home");
  },
 
  formulario:(req,res) =>{
+
    return res.render("formulario");
  }
 
 };
 
 module.exports = controlePaginas;
+
