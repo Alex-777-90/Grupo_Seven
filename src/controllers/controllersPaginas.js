47d87257ed6fd25d7fcb935269d8b2c4a4e48156
Alex <alxnvn@yahoo.com.br>
2022-07-14 22:46:11 -0300

    atualizando a rotas das páginas principais

--- src/controllers/controllersPaginas.js
+++ src/controllers/controllersPaginas.js
@@ -1,51 +1,81 @@
 const fs = require('fs');
 const path = require('path');
 
-//const farmacia inicio -------------------------------------------------------------------------------------////////
-const productsFarmaciaPath = path.join(__dirname, '../database/farmaciaDataBase.json');
-const productsFarmacia = JSON.parse(fs.readFileSync(productsFarmaciaPath, 'utf-8'));
+const allProdutosPath = path.join(__dirname, '../database/allProdutos.json'); //pega os dados do JSON
+const allProducts = JSON.parse(fs.readFileSync(allProdutosPath, 'utf-8')); // transforma o JSON e uma array 
 
-const productsFarmaciaPath2 = path.join(__dirname, '../database/farmaciaDataBase2.json');
-const productsFarmacia2 = JSON.parse(fs.readFileSync(productsFarmaciaPath2, 'utf-8'));
-
-//const farmacia fim -------------------------------------------------------------------------------------////////
-
-
-//const pet inicio -------------------------------------------------------------------------------------////////
-const productsPetPath = path.join(__dirname, '../database/petDataBase.json');
-const productsPet = JSON.parse(fs.readFileSync(productsPetPath, 'utf-8'));
-
-const productsPetPath2 = path.join(__dirname, '../database/petDataBase2.json');
-const productsPet2 = JSON.parse(fs.readFileSync(productsPetPath2, 'utf-8'));
-
-//const pet fim -------------------------------------------------------------------------------------////////
-
-
-//const variedades inicio -------------------------------------------------------------------------------------////////
-const productsVariedadesPath = path.join(__dirname, '../database/variedadesDataBase.json');
-const productsVariedades = JSON.parse(fs.readFileSync(productsVariedadesPath, 'utf-8'));
-
-const productsVariedadesPath2 = path.join(__dirname, '../database/variedadesDataBase2.json');
-const productsVariedades2 = JSON.parse(fs.readFileSync(productsVariedadesPath2, 'utf-8'));
-
-//const variedades fim -------------------------------------------------------------------------------------////////
 
 const toThousand = n => n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 
 
 const controlePaginas = {
  
+  // farmacia inicio -------------------------------------------------------------------------------------////////
   farmacia:(req,res) =>{
+
+    let productsFarmacia = allProducts.filter(function(value){
+
+      return value.sector == "farmacia" && value.item <= 3
+         
+    });
+
+
+    let productsFarmacia2 = allProducts.filter(function(value){
+
+      return value.sector == "farmacia" && value.item >= 4
+
+    });
+ 
+
     return res.render("farmacia",{productsFarmacia , productsFarmacia2, toThousand});
   },
+  // farmacia fim ----------------------------------------------------------------------------------------////////
 
+
+  // pet inicio ------------------------------------------------------------------------------------------////////
   pet:(req,res) =>{
-     return res.render("pet",{ productsPet,productsPet2,toThousand });
-  },
 
+    let productsPet = allProducts.filter(function(value){
+
+      return value.sector == "pet" && value.item <= 3
+         
+    });
+
+
+    let productsPet2 = allProducts.filter(function(value){
+
+      return value.sector == "pet" && value.item >= 4
+
+    });
+
+
+     return res.render("pet",{ productsPet, productsPet2, toThousand });
+  },
+ // pet fim -------------------------------------------------------------------------------------------////////
+
+
+// variedades inicio -----------------------------------------------------------------------------------////////
   variedades:(req,res) =>{
+
+    let productsVariedades = allProducts.filter(function(value){
+
+      return value.sector == "variedades" && value.item <= 3
+         
+    });
+
+
+    let productsVariedades2 = allProducts.filter(function(value){
+
+      return value.sector == "variedades" && value.item >= 4
+
+    });
+
+
     return res.render("variedades", {productsVariedades, productsVariedades2 ,toThousand });
   },
+ //variedades fim -------------------------------------------------------------------------------------////////
+
+
 
   home:(req,res) =>{
     return res.render("home");
